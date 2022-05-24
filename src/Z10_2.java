30e26ef95e68bcf3e0a84c3484fbbe10bf554e67
LachTomasz <lach.tomasz@gmail.com>
2022-05-24 15:31:22 +0200

    Initial commit

---
+++ src/Z10_2.java
@@ -0,0 +1,15 @@
+public class Z10_2 {
+    public static void main(String[] args) {
+
+        double Fi = (double)(1+Math.sqrt(5.0)/2);
+
+        System.out.printf("e = %.10f\n", Math.E);
+        System.out.println(String.format("e = %.10f", Math.E));
+
+        System.out.printf("Pi = %.10f\n", Math.PI);
+        System.out.println(String.format("Pi = %.10f", Math.PI));
+
+        System.out.printf("Fi = %.10f\n", Fi);
+        System.out.println(String.format("Fi = %.10f", Fi));
+    }
+}
